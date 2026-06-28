22b73a8fdb3407c0cb4c8ea112cdb4aca7342e79
Maj20PL <majewski730@gmail.com>
2026-06-28 15:30:28 +0200

    Zakoncznie projektu
    Model segmentacji oraz model wykrywania działaja

---
+++ projekt_hied/modelu_wykrywania.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+
+def train_yolo():
+    print("Inicjalizacja modelu YOLOv8 Nano...")
+    model = YOLO('yolov8n.pt')
+
+    print("\n--- ROZPOCZECIE TRENINGU YOLO (30 EPOK) ---")
+    # Trening
+    results = model.train(
+        data='trening_oko.yaml',     # Mapa klas
+        epochs=30,                   # Liczba epok
+        imgsz=512,                   # Rozmiar obrazów
+        device='cpu',                # Wybranie procesora
+        batch=20,                    # Liczba zdjęć naraz
+        project='YOLO_Oko',          # Gdzie zapisać wyniki
+        name='trening1'               # Nazwa próby
+    )
+    print("--- TRENING ZAKOŃCZONY ---")
+    print("Wytrenowany model znajduje się w: YOLO_Oko/trening1/weights/best.pt")
+
+if __name__ == '__main__':
+    train_yolo()
